f3cf5b66c3e9f40f9b64bb83fc21cb36a7bb7774
Vsevolod Petrov <vsev92@mail.ru>
2024-12-12 12:20:22 +0700

    c1

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+use Slim\Factory\AppFactory;
+
+$app = AppFactory::create();
+$app->addErrorMiddleware(true, true, true);
+
+$app->get('/', function ($request, $response) {
+    $response->getBody()->write('Welcome to Slim!');
+    return $response;
+    // Благодаря пакету slim/http этот же код можно записать короче
+    // return $response->write('Welcome to Slim!');
+});
+$app->run();
